c2acad4fa94797e028641755566fba8a510ca085
niravpatel129 <niravpatelp129@gmail.com>
2019-10-30 12:19:39 -0400

    changes to project

---
+++ src/Projects.js
@@ -0,0 +1,73 @@
+import React from "react";
+import MainContainer from "./MainContainer";
+import moment from "moment";
+import { sortBy } from "lodash";
+
+class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://newapp-team-api-2.herokuapp.com/projects")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          projects: sortBy(data, [
+            function(project) {
+              return parseInt(project.ProjectName.match(/\d+/));
+            }
+          ])
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  createTable() {
+    let key = 0;
+    const table = this.state.projects.map(project => (
+      <tr key={key++}>
+        <td>{project.ProjectName}</td>
+        <td>{project.ProjectDescription}</td>
+        <td>
+          {moment(project.ProjectStartDate)
+            .utc()
+            .format("LL")}
+        </td>
+        <td>
+          {project.ProjectEndDate === null
+            ? "n/a"
+            : moment(project.ProjectEndDate).format("LL")}
+        </td>
+      </tr>
+    ));
+    return table;
+  }
+  render() {
+    return (
+      <MainContainer sidebar="Projects">
+        <h1 className="page-header">Projects</h1>
+        <div className="row">
+          <div className="col-md-12">
+            <table className="table table-striped table-bordered">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Description</th>
+                  <th>Start Date</th>
+                  <th>End Date</th>
+                </tr>
+              </thead>
+              <tbody>{this.createTable()}</tbody>
+            </table>
+          </div>
+        </div>
+      </MainContainer>
+    );
+  }
+}
+
+export default Projects;
